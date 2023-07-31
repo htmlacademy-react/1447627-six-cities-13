6dd645cf97f7f6baa1ba389e96d4ab7c062291fe
Rustam <rustam17.sb@gmail.com>
2023-07-31 16:41:56 +0300

    refactor: make an empty favorites section component variant a separate component

--- src/components/favorites-section/favorites-section.tsx
+++ src/components/favorites-section/favorites-section.tsx
@@ -7,50 +7,54 @@ type FavoritesSectionProps = {
   places?: Place[];
 }
 
+function EmptyFavoritesSection(): React.JSX.Element {
+  return (
+    <section className={`${styles.favorites} favorites--empty`}>
+      <h1 className="visually-hidden">Favorites (empty)</h1>
+      <div className="favorites__status-wrapper">
+        <b className="favorites__status">Nothing yet saved.</b>
+        <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+      </div>
+    </section>
+  );
+}
+
 function FavoritesSection({places}: FavoritesSectionProps): React.JSX.Element {
-  if (places?.length) {
-    const placesByCity: {
-      [city: string]: Place[];
-    } = {};
-
-    places.forEach((place) => {
-      if (place.city.name in placesByCity) {
-        placesByCity[place.city.name].push(place);
-      } else {
-        placesByCity[place.city.name] = [place];
-      }
-    });
-
-    return (
-      <section className={styles.favorites}>
-        <h1 className={styles.title}>Saved listing</h1>
-        <ul className={styles.list}>
-          {Object.keys(placesByCity).map((city) => (
-            <li className={styles.locationsItems} key={city}>
-              <div className="favorites__locations locations locations--current">
-                <div className="locations__item">
-                  <a className="locations__item-link" href="#">
-                    <span>{city}</span>
-                  </a>
-                </div>
-              </div>
-              <PlacesList additionalClassName="favorites__places" places={placesByCity[city]} cardGrid="horizontal" />
-            </li>
-          ))}
-        </ul>
-      </section>
-    );
-  } else {
-    return (
-      <section className={`${styles.favorites} favorites--empty`}>
-        <h1 className="visually-hidden">Favorites (empty)</h1>
-        <div className="favorites__status-wrapper">
-          <b className="favorites__status">Nothing yet saved.</b>
-          <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
-        </div>
-      </section>
-    );
+  if (!places || places.length === 0) {
+    return <EmptyFavoritesSection />;
   }
+
+  const placesByCity: {
+    [city: string]: Place[];
+  } = {};
+
+  places.forEach((place) => {
+    if (place.city.name in placesByCity) {
+      placesByCity[place.city.name].push(place);
+    } else {
+      placesByCity[place.city.name] = [place];
+    }
+  });
+
+  return (
+    <section className={styles.favorites}>
+      <h1 className={styles.title}>Saved listing</h1>
+      <ul className={styles.list}>
+        {Object.keys(placesByCity).map((city) => (
+          <li className={styles.locationsItems} key={city}>
+            <div className="favorites__locations locations locations--current">
+              <div className="locations__item">
+                <a className="locations__item-link" href="#">
+                  <span>{city}</span>
+                </a>
+              </div>
+            </div>
+            <PlacesList additionalClassName="favorites__places" places={placesByCity[city]} cardGrid="horizontal" />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default FavoritesSection;
